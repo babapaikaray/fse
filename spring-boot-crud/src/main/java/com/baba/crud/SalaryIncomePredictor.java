d19fd3b94e7b308daae7110c6b72cd27714bbf85
babapaikaray <52294413+babapaikaray@users.noreply.github.com>
2019-07-06 13:15:07 +0530

    commit on 6th July 2019

--- spring-boot-crud/src/main/java/com/baba/crud/SalaryIncomePredictor.java
+++ spring-boot-crud/src/main/java/com/baba/crud/SalaryIncomePredictor.java
@@ -53,17 +53,20 @@ public class SalaryIncomePredictor {
 
 	public static List<String> validateInput(InputParams ip) {
 		List<String> errorMsg = new ArrayList<String>();
-		if(null != ip.getStartSal() 
-				&& (!isNumeric(ip.getStartSal()) || new Float(ip.getStartSal()) < 1.0F)) {
+		if(null != ip.getStartSal() && (!isNumeric(ip.getStartSal()) || new Float(ip.getStartSal()) < 1.0F)) {
 			errorMsg.add("Error! \nStarting salary cannot be less than 1");
 		}
-		if(null != ip.getIncrementPer() && !StringUtils.isNumeric(ip.getIncrementPer())
-				&& (!isNumeric(ip.getIncrementPer()) || new Float(ip.getIncrementPer()) < 0.0F)) {
+		if(null != ip.getIncrementPer() && (!isNumeric(ip.getIncrementPer()) || new Float(ip.getIncrementPer()) < 0.0F)) {
 			errorMsg.add("Error! \nIncrement percent cannot be less than 0");
 		}
-		if(null != ip.getDeductionPer() && !StringUtils.isNumeric(ip.getDeductionPer())
-				&& (!isNumeric(ip.getDeductionPer()) || new Float(ip.getDeductionPer()) < 0.0F)) {
+		if(null != ip.getDeductionPer() && (!isNumeric(ip.getDeductionPer()) || new Float(ip.getDeductionPer()) < 0.0F)) {
 			errorMsg.add("Error! \nDecrement percent cannot be less than 0");
+		}
+		if(null != ip.getFreqOfIncrement() && (!isNumeric(ip.getFreqOfIncrement()) || new Float(ip.getFreqOfIncrement()) < 1.0F)) {
+			errorMsg.add("Error! \nFrequency of Increment cannot be less than 1");
+		}
+		if(null != ip.getFreqOfDeductions() && (!isNumeric(ip.getFreqOfDeductions()) || new Float(ip.getFreqOfDeductions()) < 1.0F)) {
+			errorMsg.add("Error! \nFrequency of Deductions cannot be less than 1");
 		}
 		return errorMsg;
 	}
@@ -73,26 +76,17 @@ public class SalaryIncomePredictor {
 		Float startSal = new Float(ip.getStartSal());
 		Float incrementPer = new Float(ip.getIncrementPer());
 		int predictionYrs = Integer.parseInt(ip.getPredictionForYrs());
-		int freqOfIncr = 0;
+		int freqOfIncr = Integer.parseInt(ip.getFreqOfIncrement());
 		Float incrementAmt = 0.0F;
 		List<IncrementReport> lst = new ArrayList<>();		
 
-		if(ip.getFreqOfIncrement().equalsIgnoreCase("QUARTERLY")) {
-			freqOfIncr = Constants.QUARTERLY;			
-		}else if(ip.getFreqOfIncrement().equalsIgnoreCase("HALF-YEARLY")) {
-			freqOfIncr = Constants.HALF_YEARLY;
-		}else if(ip.getFreqOfIncrement().equalsIgnoreCase("YEARLY")) {
-			freqOfIncr = Constants.YEARLY;
-		}else if(ip.getFreqOfIncrement().equalsIgnoreCase("MONTHLY")) {
-			freqOfIncr = Constants.MONTHLY;
-		}
 		System.out.println("\n\n--Increment Report----");
 		System.out.println("Year  Starting Salary  No.ofIncrements  % Of Increment  Increment Amount");
 		for(int i =1; i <= predictionYrs; i++) { 
 			IncrementReport ir = new IncrementReport();
 			incrementAmt = 0.0F;
 			for(int j=0;j < freqOfIncr;j++ ) {				
-				incrementAmt = incrementAmt+(startSal*incrementPer);
+				incrementAmt = incrementAmt+(startSal*incrementPer/100);
 			}
 			ir.setStartingSal(startSal);
 			startSal = startSal +incrementAmt;
@@ -109,30 +103,22 @@ public class SalaryIncomePredictor {
 	public static List<DeductionReport>  prepareDeductionReport(InputParams ip,List<IncrementReport> ir) {		
 
 
-		Float deductionPer = new Float(ip.getDeductionPer())/100;
+		Float deductionPer = new Float(ip.getDeductionPer());
 		int predictionYrs = Integer.parseInt(ip.getPredictionForYrs());
-		int freqOfDeduction = 0;
+		int freqOfDeduction = Integer.parseInt(ip.getFreqOfDeductions());
 		Float deductionAmt = 0.0F;
 		List<DeductionReport> lst = new ArrayList<>();		
 
-		if(ip.getFreqOfIncrement().equalsIgnoreCase("QUARTERLY")) {
-			freqOfDeduction = Constants.QUARTERLY;			
-		}else if(ip.getFreqOfIncrement().equalsIgnoreCase("HALF-YEARLY")) {
-			freqOfDeduction = Constants.HALF_YEARLY;
-		}else if(ip.getFreqOfIncrement().equalsIgnoreCase("YEARLY")) {
-			freqOfDeduction = Constants.YEARLY;
-		}else if(ip.getFreqOfIncrement().equalsIgnoreCase("MONTHLY")) {
-			freqOfDeduction = Constants.MONTHLY;
-		}
 		System.out.println("\n\n--Deduction Report----");
 		System.out.println("Year  Starting Salary  No.ofDeductions  % Of Deduction  Deduction Amount");
 		for(int i =1; i <= predictionYrs; i++) { 
 			DeductionReport dr = new DeductionReport();
 			deductionAmt = 0.0F;
+			Float startSal = ir.get(i-1).getStartingSal();
 			for(int j=0;j < freqOfDeduction;j++ ) {				
-				deductionAmt = deductionAmt+(ir.get(i-1).getStartingSal()*deductionPer);
+				deductionAmt = deductionAmt+(startSal*deductionPer/100);
 			}
-			dr.setStartingSal(ir.get(i-1).getStartingSal());			
+			dr.setStartingSal(startSal);			
 			dr.setDeductionPer(deductionPer);
 			dr.setDeductionAmt(deductionAmt);
 			dr.setNoOfDeductions(String.valueOf(freqOfDeduction));
@@ -149,18 +135,20 @@ public class SalaryIncomePredictor {
 
 
 		System.out.println("\n\n--Prediction Report----");
-		System.out.println("Year  Starting Salary  Increment Amount  Deduction Amount  Growth");
+		System.out.println("Year  Starting Salary  Increment Amount  Deduction Amount           Growth");
 		int i = 1;
 
+		Float startSal = ir.get(0).getStartingSal();
 		for(IncrementReport irR: ir ) {		
 			DeductionReport drObj = dr.get(i-1);
 			PredictionReport pr = new PredictionReport();
 			pr.setDeductiontAmt(drObj.getDeductionAmt());
 			pr.setIncrementAmt(irR.getIncrementAmt());
-			pr.setGrowthPer((((irR.getStartingSal()+(irR.getIncrementAmt() - drObj.getDeductionAmt()))/irR.getStartingSal())*100)-100);
-			pr.setStartingSal(irR.getStartingSal());
+			pr.setGrowthPer(pr.getIncrementAmt() - pr.getDeductiontAmt());
+			pr.setStartingSal(startSal);
+			startSal = startSal + pr.getGrowthPer();
 
-			System.out.println(i+"	"+pr.getStartingSal()+"		 "+pr.getIncrementAmt()+"		 "+pr.getDeductiontAmt()+" 		"+pr.getGrowthPer());
+			System.out.println(i+"	"+pr.getStartingSal()+"		 "+pr.getIncrementAmt()+"		 "+pr.getDeductiontAmt()+" 		          "+pr.getGrowthPer());
 			i++;
 		}		
 		return lst;		
